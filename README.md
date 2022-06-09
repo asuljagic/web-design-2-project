c13446926b9e218c22ebe7631d6859359c088571
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2022-06-09 12:48:42 +0200

    Deployed to Heroku

---
+++ README.md
@@ -0,0 +1,14 @@
+### Web design project with backend in NodeJS
+
+#### Technologies used
+
+1. Frontend - html, css, bootstrap
+2. Backend - node js, express js, authentication - jwt
+3. Database - MongoDB
+
+#### How to Start
+
+1. Clone the repo. 
+2. Run `npm install`
+3. Run `node server.js`
+4. Server will be running at `locahost:5000`
---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const connectDB = require("./db");
+const app = express();
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const { adminAuth, userAuth } = require("./middleware/auth.js");
+const PORT = process.env.PORT || 5000;
+const User = require("./model/User");
+
+app.set("view engine", "ejs");
+connectDB();
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET'
+}))
+
+function isLoggedIn(req,res,next) {
+  req.user ? next() : res.sendStatus(401);
+}
+
+var passport = require('passport');
+var userProfile;
+ 
+app.use(passport.initialize());
+app.use(passport.session());
+// Routes
+app.use("/api/auth", require("./Auth/route"));
+
+app.get("/", (req, res) => res.render("home")); 
+app.get("/register", (req, res) => res.render("register"));
+app.get("/login", (req, res) => res.render("login"));
+app.get("/logout", (req, res) => {
+  res.cookie("jwt", "", { maxAge: "1" });
+  req.logout(() => {
+    req.session.destroy();
+  })
+  res.redirect("/login");
+});
+
+  
+app.get("/google", isLoggedIn, (req,res) => res.render("google", {user: userProfile}))
+app.get("/admin", adminAuth, (req, res) => res.render("admin"));
+app.get("/basic", userAuth, (req, res) => res.render("user", {user: userProfile}));
+app.get('/error', (req, res) => res.send("error logging in"));
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+ 
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+/*  Google AUTH  */
+ 
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_CLIENT_ID = '651708385881-ulcgq3rfjanhb02g9uvitrq3efk1hj1f.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('/google');
+  });
+
+const server = app.listen(PORT, () =>
+  console.log(`Server Connected to port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err) => {
+  console.log(`An error occurred: ${err.message}`);
+  server.close(() => process.exit(1));
+});
