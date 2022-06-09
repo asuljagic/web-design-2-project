d0771295febd097665dc26da4e91f090efde5e61
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2022-06-09 13:25:57 +0200

    switched from compass to atlas

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Mongoose = require("mongoose");
 
-const localDB = `mongodb://localhost:27017/role_auth`;
-//const localDB = 'mongodb+srv://asuljagic1:sarajevo2022@clusterwebdes2.aihhbit.mongodb.net/?retryWrites=true&w=majority';
+//const localDB = `mongodb://localhost:27017/role_auth`;
+const localDB = 'mongodb+srv://asuljagic1:sarajevo2022@clusterwebdes2.aihhbit.mongodb.net/?retryWrites=true&w=majority';
 
 const connectDB = async () => {
   await Mongoose.connect(localDB, {
