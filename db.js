cfc74886beb5818c7404465b3218fed14312fd40
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2022-06-09 14:21:14 +0200

    updated

--- db.js
+++ db.js
@@ -4,10 +4,10 @@ const Mongoose = require("mongoose");
 const localDB = 'mongodb+srv://asuljagic1:sarajevo2022@clusterwebdes2.aihhbit.mongodb.net/?retryWrites=true&w=majority';
 
 const connectDB = async () => {
-  await Mongoose.connect(localDB, {
+  await Mongoose.connect(localDB/*, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  });
+  }*/);
 
   console.log("MongoDB Connected");
 };
