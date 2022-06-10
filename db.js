447395c5f070b81436bb6210c4c83c09cc3b09f4
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2022-06-10 10:23:59 +0200

    v7

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Mongoose = require("mongoose");
 
-const localDB = `mongodb://127.0.0.1:27017/role_auth`;
-
+//const localDB = `mongodb://127.0.0.1:27017/role_auth`;
+const localDB = process.env.MONGO_URL;
 
 const connectDB = async () => {
   await Mongoose.connect(localDB, {
