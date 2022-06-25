911aa677659183e991aef7f25064fa231a2d45fb
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2022-06-25 15:23:11 +0200

    fixed position of welcome message

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const Mongoose = require("mongoose");
+require('dotenv').config({ path: 'MONGO_URL' });
 
 //const localDB = `mongodb://127.0.0.1:27017/role_auth`;
 const localDB = process.env.MONGO_URL;
+//const localDB = `mongodb+srv://asuljagic1:sarajevo2022@clusterwebdes2.aihhbit.mongodb.net/?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
   await Mongoose.connect(localDB, {
